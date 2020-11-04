f416624a3fe750c86be0c4d1642475607b5c0f19
Boyy2002 <64878446+Boyy2002@users.noreply.github.com>
2020-11-04 14:36:40 +0000

    added code and update ReadMe

--- ALC.py
+++ ALC.py
@@ -1,1 +1,59 @@
-print("hello world")
+import musicbrainzngs
+import sys
+import requests
+import json
+
+
+musicbrainzngs.set_useragent(
+    "python-musicbrainzngs-example",
+    "0.1",
+    "https://github.com/alastair/python-musicbrainzngs/",
+)
+
+print("please insert the name of the artist")
+
+inputArtist = input() 
+
+artists = musicbrainzngs.search_artists(artist=inputArtist)
+if (artists['artist-count'] == 0):
+    print('artist not found')
+    sys.exit()
+
+artist = artists['artist-list'][0]
+print(u"artist found {name}".format(name=artist["name"]))
+
+allSongs = set()
+
+currentOffset = 0
+recordings = musicbrainzngs.browse_recordings(artist=artist['id'], limit=100, offset=0)
+while(len(recordings["recording-list"]) > 0):
+    for recording in recordings["recording-list"]:
+        allSongs.add(recording["title"])
+               
+    currentOffset = currentOffset + 100
+    recordings = musicbrainzngs.browse_recordings(artist=artist['id'], limit=100, offset=currentOffset)
+
+print('songs for the artist')
+totalvalidsong = 0
+totalvalidlyrics = 0
+for song in sorted(allSongs): 
+    print(song)
+
+    address = ('https://api.lyrics.ovh/v1/' + inputArtist +'/' + song)
+    
+    response = requests.get(address) 
+    if response.status_code == 200:
+        lyrics = response.json() ["lyrics"]
+        lyricsCount = len(lyrics.split())
+        print(lyricsCount) 
+        if lyricsCount > 0:
+            totalvalidsong += 1
+            totalvalidlyrics += lyricsCount
+    else  :
+        print("Failed")
+
+average = totalvalidlyrics / totalvalidsong
+Message = ("The average number of lyrics for " + artist["name"] +" is " + str(average)) 
+
+with open('Message.txt', 'w') as outfile:
+    json.dump(Message, outfile)
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Create a CLI App to return average number of lyrics for a given Artist
 Run the below to load the required packages
     pip install -r requirements.txt 
 
-Open ALC.py and enter Artist see Print out
+Open ALC.py and enter Artist see Message.txt file
